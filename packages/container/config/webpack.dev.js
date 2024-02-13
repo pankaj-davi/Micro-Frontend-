c4a6d92ffcf56149a60d8e188d4b46f5ffcb259e
pankaj-davi <menariapankaj17@gmail.com>
2024-02-13 16:41:25 +0530

    container deploy script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,5 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
@@ -14,16 +14,14 @@ const devConfig = {
     },
     plugins: [
         new ModuleFederationPlugin({
-            name : 'container',
-            remotes : {
-                marketing : 'marketing@http://localhost:8081/remoteEntry.js'
+            name: 'container',
+            remotes: {
+                marketing: 'marketing@http://localhost:8081/remoteEntry.js'
             },
-            shared : packageJson.dependencies
+            shared: packageJson.dependencies
         }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        })
+
     ]
 }
 
-module.exports = merge(commonConfig , devConfig)
+module.exports = merge(commonConfig, devConfig)
